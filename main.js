07a61e54cb6df96cf4abde2471c5caa0c09a2cc1
tabiqchohan <160527853+tabiqchohan@users.noreply.github.com>
2024-03-10 11:09:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var frientname = ["salman", "daniyal", "saqib", "naveed"];
+var message = ("kia haal han");
+console.log("salam", frientname[0], message);
+console.log("salam", frientname[1], message);
+console.log("salam", frientname[2], message);
+console.log("salam", frientname[3], message);
---
+++ main.ts
@@ -0,0 +1,6 @@
+let frientname=["salman","daniyal","saqib","naveed"]
+let message=("kia haal han")
+console.log("salam",frientname[0],message)
+console.log("salam",frientname[1],message)
+console.log("salam",frientname[2],message)
+console.log("salam",frientname[3],message)
